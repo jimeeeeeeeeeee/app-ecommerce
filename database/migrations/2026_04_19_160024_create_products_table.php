fd386435fa253d9134094f36a8051d069ec008c6
jimeeeeeeeeeee <jimenapadilabonilla@gmail.com>
2026-04-19 10:29:55 -0600

    Creación de tabas categories y products

---
+++ database/migrations/2026_04_19_160024_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->text('descripción')->nullable();
+            $table->decimal('precio_men', 8, 2);
+            $table->decimal('precio_may', 8, 2)->nullable();
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->enum('status', ['activo', 'inactivo'])->default('activo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
