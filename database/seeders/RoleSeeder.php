b3f65b71eef3dac4e44b070fbfe8a47f2897bd19
jimeeeeeeeeeee <jimenapadilabonilla@gmail.com>
2026-04-26 11:05:49 -0600

    Creación de seeder User

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,13 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        /*Definicion de roles*/
+        $roles = ['super_admin', 'admin', 'user', 'vendedor', 'marketing', 'atencion_cliente'];
+
+        /*Crear si no existe*/
+        foreach($roles as $roleName)
+            {
+                Role::UpdateOrCreate(['nombre' => $roleName]);
+            }
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +14,14 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $user = User::updateOrCreate(
+            ['email' => 'sa@gmail.com'],
+            ['name' => 'Super Admin'],
+            ['password' => bcrypt('12345678')]
+        );
+
+        $superAdminRole = Role::where('name', 'Super Admin')->first();
+
+        $user -> roles() ->syncWithoutDetaching($superAdminRole);
     }
 }
